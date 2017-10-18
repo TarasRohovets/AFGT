53f95ebc151ad234e87d1dd6acb60035b3a9aaed
TarasRohovets <31953701+TarasRohovets@users.noreply.github.com>
2017-10-18 09:43:21 +0000

    commit inicial

---
+++ AFGT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AFGT.Startup))]
+namespace AFGT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
