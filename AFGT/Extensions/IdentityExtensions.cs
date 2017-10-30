4c24f0cbe84612f8e93c9151e601189337f70c49
TarasRohovets <31953701+TarasRohovets@users.noreply.github.com>
2017-10-30 16:47:49 +0000

    Adicionei campo extra no identity
    fdfdf

---
+++ AFGT/Extensions/IdentityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace AFGT.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetNameUser(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("NameUser");
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
--- AFGT/Models/IdentityModels.cs
+++ AFGT/Models/IdentityModels.cs
@@ -16,8 +16,11 @@ namespace AFGT.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim("NameUser", this.NameUser.ToString()));
             return userIdentity;
         }
+
+        public string NameUser { get; set; }// Adicionei uma extra property
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, CustomRole,
