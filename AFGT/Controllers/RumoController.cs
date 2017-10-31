273af31755c6d002f620c150a8d7ee157cea35b8
Aline <alinessnascimento1@gmail.com>
2017-10-31 19:13:17 +0000

    Request PointA

--- AFGT/Controllers/RumoController.cs
+++ AFGT/Controllers/RumoController.cs
@@ -13,11 +13,12 @@ namespace AFGT.Controllers
         // GET: Rumo
         private afgtEntities db = new afgtEntities();
 
-        public ActionResult Index(String PointA)
+        public ActionResult Index()
         {
             //Evento evento = db.Eventos.Find(id);
             string url1 = "https://www.google.com/maps/embed/v1/directions?key=";
             string key = WebConfigurationManager.AppSettings["GoogleMapsAPIKey"];
+            string PointA = Request.Form["GoogleMapsAPIKey"].ToString();
             string PointB = "Lisboa";//evento.Morada.ToString();
 
             if (PointA != null)
@@ -25,7 +26,7 @@ namespace AFGT.Controllers
                 ViewBag.Link = url1 + key + "&origin=" + PointA + "&destination=" + PointB;
             }
             else {
-                ViewBag.Link = url1 + key + "&origin=" + "Coimbra" + "&destination=" + PointB;
+                ViewBag.Link = "https://www.google.com/maps/embed/v1/place?key=" + key + " &q=" + PointB;
             }
 
             return View();
