f69aed94986584a262e2261bcd1689a1bfc513ff
ALTAR-VM2\altar16 <not@using.this>
2017-11-02 10:16:59 +0000

    tatara

--- AFGT/Controllers/HomeController.cs
+++ AFGT/Controllers/HomeController.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using SpotifyWebAPI.SpotifyModel;
+
 using System.Threading.Tasks;
 
 namespace AFGT.Controllers
