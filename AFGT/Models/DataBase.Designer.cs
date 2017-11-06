8e5e3069d857c8e0388c8f3dec0557936c93d423
DESKTOP-LJ7PAMB\Deathangel <francisco.d.serragmail.com>
2017-11-06 12:30:26 +0000

    teste

--- AFGT/Models/DataBase.Designer.cs
+++ AFGT/Models/DataBase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\altar16\Documents\AFGT\AFGT\Models\DataBase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Deathangel\Documents\AFGT\AFGT\Models\DataBase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
