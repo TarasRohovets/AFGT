40fe9186bfc790a11d24bf33d8830656c35cd926
TarasRohovets <31953701+TarasRohovets@users.noreply.github.com>
2017-11-08 15:33:28 +0000

    commit2

--- AFGT/Models/AccountViewModels.cs
+++ AFGT/Models/AccountViewModels.cs
@@ -81,6 +81,37 @@ namespace AFGT.Models
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
+    public class RegisterOrgViewModel
+    {
+
+        [Display(Name = "UserRoles")]
+        public string UserRoles { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "NIPC")]
+        public int NIPC { get; set; }   //isto foi a propriedade extra
+
+    }
 
     public class ResetPasswordViewModel
     {
