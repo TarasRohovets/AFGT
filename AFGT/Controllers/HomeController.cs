8a9991fa22bbc3587cd05b247192fecdc55de192
Aline <alinessnascimento1@gmail.com>
2017-11-10 15:06:06 +0000

    new filtragem

--- AFGT/Controllers/HomeController.cs
+++ AFGT/Controllers/HomeController.cs
@@ -14,49 +14,67 @@ namespace AFGT.Controllers
     {
         private afgtEntities db = new afgtEntities();
 
-        List<SelectListItem> list = new List<SelectListItem>
+        List<SelectListItem> listPesquisa = new List<SelectListItem>
             {
                 new SelectListItem { Value = "1", Text = "Artista" },
-                new SelectListItem { Value = "2", Text = "Estilo Musical" },
-                new SelectListItem { Value = "3", Text = "Data" },
-                new SelectListItem { Value = "4", Text = "Cidade" }
+                new SelectListItem { Value = "2", Text = "Estilo Musical" }
+            };
+
+        List<SelectListItem> listOpcao = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Data" },
+                new SelectListItem { Value = "2", Text = "Cidade" }
             };
 
         public ActionResult Index()
         {
             var result = db.Eventos.OrderBy(evento => evento.Data).ToList();
-            ViewBag.ListaPesquisa = list;
+
+            ViewBag.ListaPesquisa = listPesquisa;
+            ViewBag.ListaOpcao = listOpcao;
+
             return View(result);
         }
 
         [HttpPost]
-        public ActionResult Index(string ConteudoPesquisa, int? GeneroMusicalID, string ListaPesquisa, string Dia, string PointA, string TipoOpcao)
+        public ActionResult Index(string ConteudoPesquisa, int? GeneroMusicalID, string ListaPesquisa, string Dia, string PointA, string ListaOpcao)
         {
             List<Evento> Evento = new List<Evento>();
 
-            var evento = db.Eventos.OrderBy(e => e.Data);
-            var result = evento.ToList();
+            var evento = db.Eventos.OrderBy(e => e.Data).ToList();
+            var result = db.Eventos.ToList();
 
-            ViewBag.ListaPesquisa = list;
+            ViewBag.ListaPesquisa = listPesquisa;
+            ViewBag.ListaOpcao = listOpcao;
 
             switch (ListaPesquisa)
             {
                 case "1":
-
                     if (ConteudoPesquisa == "")
                     {
-                        result = evento.ToList();
+                        result = db.Eventos.ToList();
                     }
-                    if (TipoOpcao == "Local")
+                    if (ListaOpcao == "Local")
                     {
-                        evento = db.Eventos.OrderBy(e => e.Morada.Cidade);
-                        result = evento.Include(e => e.Artistas).Where(e => e.Artistas.Any(a => a.Nome.ToLower() == ConteudoPesquisa.ToLower())).ToList();
+                        foreach ( Evento item in evento) {
+                            if (item.Morada.Cidade.ToLower() == PointA.ToLower() && item.Artistas.Any(a => a.Nome.ToLower() == ConteudoPesquisa.ToLower()))
+                            {
+                                Evento.Add(item);
+                            };
+                        }
+                        result = Evento;
                     }
                     else
                     {
-                        result = evento.Include(e => e.Artistas).Where(e => e.Artistas.Any(a => a.Nome.ToLower() == ConteudoPesquisa.ToLower())).ToList();
+                        foreach (Evento item in evento)
+                        {
+                            if (item.Data.Value.ToString("yyyy-MM-dd") == Dia && item.Artistas.Any(a => a.Nome.ToLower() == ConteudoPesquisa.ToLower()))
+                            {
+                                Evento.Add(item);
+                            };
+                        }
+                        result = Evento;
                     }
-
                     break;
 
                 case "2":
@@ -65,38 +83,27 @@ namespace AFGT.Controllers
                     {
                         result = evento.ToList();
                     }
-                    if (TipoOpcao == "Local")
+                    if (ListaOpcao == "Local")
                     {
-                        evento = db.Eventos.OrderBy(e => e.Morada.Cidade);
-                        result = evento.Include(e => e.Artistas).Where(e => e.Artistas.Any(a => a.GeneroMusicalID == GeneroMusicalID)).ToList();
+                        foreach (Evento item in evento)
+                        {
+                            if (item.Morada.Cidade.ToLower() == PointA.ToLower() && item.Artistas.Any(a => a.GeneroMusicalID == GeneroMusicalID))
+                            {
+                                Evento.Add(item);
+                            };
+                        }
+                        result = Evento;
                     }
                     else
                     {
-                        result = evento.Include(e => e.Artistas).Where(e => e.Artistas.Any(a => a.GeneroMusicalID == GeneroMusicalID)).ToList();
-                    }
-                    break;
-
-                case "3":
-                    if (Dia == "")
-                    {
-                        result = evento.ToList();
-                    }
-                    else
-                    {
-                        result = evento.ToList().Where(model => model.Data.Value.ToString("yyyy-MM-dd") == Dia).ToList();
-                    }
-                    break;
-
-                case "4":
-                    evento = db.Eventos.OrderBy(e => e.Morada.Cidade);
-                    if (PointA == "")
-                    {
-                        
-                        result = evento.ToList();
-                    }
-                    else
-                    {
-                        result = evento.ToList().Where(model => model.Morada.Cidade.ToLower() == PointA.ToLower()).ToList();
+                        foreach (Evento item in evento)
+                        {
+                            if (item.Data.Value.ToString("yyyy-MM-dd") == Dia && item.Artistas.Any(a => a.GeneroMusicalID == GeneroMusicalID))
+                            {
+                                Evento.Add(item);
+                            };
+                        }
+                        result = Evento;
                     }
                     break;
             }
